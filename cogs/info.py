b0c832f1d632f490a15c2d4cb002048ec5f0bc74
zero <suhasvoolla@gmail.com>
2021-04-05 19:20:20 -0500

    feat: ping command

--- cogs/info.py
+++ cogs/info.py
@@ -1,10 +1,19 @@
 import discord
 from discord.ext import commands
+import time
 
 class Info(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
   
+  @commands.command()
+  async def ping(self, ctx):
+    """ Gets the bot's ping """
+    before = time.monotonic()
+    before_ws = int(round(self.bot.latency * 1000, 1))
+    message = await ctx.send("🏓 Pong?")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"🏓 WS: {before_ws}ms  |  REST: {int(ping)}ms")
 
 def setup(bot):
   bot.add_cog(Info(bot))
