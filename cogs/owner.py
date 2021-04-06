11f1a9b060525a2ae2509a1edc3c56edd0be5a17
zero <suhasvoolla@gmail.com>
2021-04-05 19:36:00 -0500

    feat: add cog reload command

---
+++ cogs/owner.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import importlib
+
+from discord.ext.commands.core import command
+
+class Owner(commands.Cog):
+    """ Commands for the bot owner to use. """
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.is_owner()
+    @commands.command()
+    async def reload(self, ctx, cog):
+        """ Reloads the given cog. """
+        try:
+            self.bot.reload_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Something happened... \n```py\n{e.__traceback__}\n```")
